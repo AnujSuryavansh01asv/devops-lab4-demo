49b23dcce3621096f406222fe42a531f98e32236
AnujSuryavansh01asv <2021wa86448@wilp.bits-pilani.ac.in>
2025-07-11 15:21:42 +0530

    Updated App.java from local

--- src/main/java/com/devops/lab/App.java
+++ src/main/java/com/devops/lab/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("BITS ID ->  2021WA86448");	
+        System.out.println("Anuj");
     }
 }
